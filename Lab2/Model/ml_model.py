72827289e90e1e2eb5ceebdddee0d76f848a4db0
MrExiDHA <mrexidha@mail.ru>
2021-01-11 13:15:23 +0300

    Delete redundant

--- Lab2/Model/ml_model.py
+++ Lab2/Model/ml_model.py
@@ -5,11 +5,9 @@ best_clf_filename = "best_clf.dodo"
 
 class MlModel(object):
     _model = None
-    _modelName = ''
 
     def __init__(self):
         loaded_model = load(open(best_clf_filename, 'rb'))
-        self._modelName = loaded_model[0]
         self._model = loaded_model[1]
 
     def predict(self, x_data):
